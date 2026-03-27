638c63b48cf86e189d7e62fa96b91111be321019
Douglas <Douglas.rtoledo2003@gmail.com>
2026-03-27 16:12:11 -0300

    feat: request property in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react"
+import type { IMovie } from "./@types/movie"
+import { MovieAPI } from "./services/api"
+import { MovieCard } from './components/movie/movieCard'
+
 
 function App() {
+  const [list, setList] = useState<IMovie[]>([])
+
+  useEffect(() => {
+    MovieAPI.getAll()
+      .then(data => {setList(data)  
+    })
+  }, []);
 
   return (
     <>
       <div>
         <h1>Hello World!</h1>
+        <div>
+          {list.map(movie => <MovieCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            overview={movie.overview} 
+            poster_path={movie.poster_path} 
+          />)
+          }
+        </div>
       </div>
     </>
   )
