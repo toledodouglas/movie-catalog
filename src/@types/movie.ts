6f8336a3164399e0106d4fddad061b2fa6184d10
Douglas <Douglas.rtoledo2003@gmail.com>
2026-03-27 16:10:54 -0300

    feat: create new interface

--- src/@types/movie.ts
+++ src/@types/movie.ts
@@ -1,9 +1,16 @@
 
-interface IMovie {
+export interface IMovie {
     id: number;
     title: string;
     overview: string
     poster_path: string;
     vote_average: number;
     release_date: string;
+}
+
+export interface IMovieItemProps{
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
 }
